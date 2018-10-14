a0851b70b52c0993119efeea56fddb705c5c06f1
KirillKostylev <kirillkostylev2000@gmail.com>
2018-10-15 00:04:18 +0300

    last update

--- src/OutputData.java
+++ src/OutputData.java
@@ -1,7 +1,4 @@
 public class OutputData {
-    public static void outputData(String txt, int number) {
-        System.out.println(txt + number);
-    }
     public static void outputData(String txt){
         System.out.println(txt);
     }
